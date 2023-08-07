07fc929ff94e1810b9f42a4ee1fa5742f24482cf
Iridium <moisxphillip@gmail.com>
2023-08-06 21:51:02 -0300

    Adding typedefs for posterior use

---
+++ engine/lib/Typedef.hpp
@@ -0,0 +1,25 @@
+#ifndef TYPEDEF_LICHEN
+#define TYPEDEF_LICHEN
+
+#include <cstdint>
+
+//no hurry to impose these...
+
+typedef int8_t int8;
+typedef uint8_t uint8;
+
+typedef int16_t int16;
+typedef uint16_t uint16;
+
+typedef int32_t int32;
+typedef uint32_t uint32;
+
+typedef int64_t int64;
+typedef uint64_t uint64;
+
+typedef float fp32;
+typedef double fp64;
+typedef long double fp80;
+
+
+#endif//TYPEDEF_LICHEN
